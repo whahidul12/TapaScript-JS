b19373a1d11f02a9047d2cced9b3e318a27b4574
whahidul12 <mdislamwhahid@gmail.com>
2025-09-01 21:25:57 +0600

    add cv

--- day004_if-else.js
+++ day004_if-else.js
@@ -1,2 +1,18 @@
 const num = 12;
 num === 12 ? console.log('yes') : console.log('no');
+const city = "New Yrk";
+switch (city) {
+  case "Bangalore":
+  case "Kolkata":
+  case "Agra":
+  case "Jaipur":
+    console.log("All these are in India");
+    break;
+  case "New York":
+  default:
+    console.log("It is in USA");
+}
+
+for (let i = 1, j = 10, k = 100; i <= 10 && j >= 1; i++, j--, k++) {
+  console.log(i, j, k);
+}
